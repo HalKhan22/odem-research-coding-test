d8f8f7f6a650ac6ee7f14167f12efc7c85f01cc6
Haleema Khatoon <86432187+HalKhan22@users.noreply.github.com>
2023-12-22 22:44:15 +0000

    Update FileReadWrite.java

--- src/main/java/com/coding/challenge/InputOutput/FileReadWrite.java
+++ src/main/java/com/coding/challenge/InputOutput/FileReadWrite.java
@@ -12,6 +12,8 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class FileReadWrite {
 
@@ -37,7 +39,7 @@ public class FileReadWrite {
         return inputRecordList;
     }
 
-    public void writeCsvFromArrayList(List<OutputRecord> outputRecords){
+    public static void writeCsvFromArrayList(List<OutputRecord> outputRecords){
 
         try (CSVWriter csvWriter = new CSVWriter(new FileWriter("src/main/resources/output.csv"))) {
             // Write data
@@ -46,6 +48,53 @@ public class FileReadWrite {
             }
 
             System.out.println("Data has been written to output.csv");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void processLineByLineFromInput(String csvFlePath){
+        try (BufferedReader br = new BufferedReader(new FileReader(csvFlePath))) {
+            String line;
+
+            Map<String, OutputRecord> outputRecords = new TreeMap<>();
+
+            while ((line = br.readLine()) != null) {
+                String[] fields = line.split(",");
+                Long timestamp = Long.parseLong(fields[0]);
+                String symbol = fields[1];
+                int quantity = Integer.parseInt(fields[2]);
+                int price = Integer.parseInt(fields[3]);
+
+                InputRecord currInput = new InputRecord(timestamp, symbol, quantity,price);
+                if(outputRecords.get(currInput.getSymbol()) == null){
+                    OutputRecord outputRecord = new OutputRecord();
+                    outputRecord.setMaxTimeGap(0);
+                    outputRecord.setTotalVolumeTraded(currInput.getQuantity());
+                    outputRecord.setMaxTradePrice(currInput.getPrice());
+                    outputRecord.setNumerator((currInput.getPrice()*currInput.getQuantity()));
+                    outputRecord.setDenominator( currInput.getQuantity());
+                    outputRecord.setSymbol(currInput.getSymbol());
+                    outputRecord.setPrevTimestamp(currInput.getTimestamp());
+                    outputRecords.put(currInput.getSymbol(),outputRecord);
+                }else{
+                    OutputRecord outputRecord = outputRecords.get(currInput.getSymbol());
+                    outputRecord.setMaxTimeGap(Math.max(outputRecord.getMaxTimeGap(),(int)(currInput.getTimestamp()- outputRecord.getPrevTimestamp())));
+                    outputRecord.setTotalVolumeTraded(outputRecord.getTotalVolumeTraded()+currInput.getQuantity());
+                    outputRecord.setMaxTradePrice(Math.max(outputRecord.getMaxTradePrice(),currInput.getPrice()));
+                    outputRecord.setNumerator(outputRecord.getNumerator() + (currInput.getPrice()*currInput.getQuantity()));
+                    outputRecord.setDenominator(outputRecord.getDenominator() + currInput.getQuantity());
+                    outputRecord.setSymbol(currInput.getSymbol());
+                    outputRecord.setPrevTimestamp(currInput.getTimestamp());
+                    outputRecords.put(currInput.getSymbol(),outputRecord);
+                }
+
+            }
+
+            List<OutputRecord> outputRecordList = new ArrayList<>(outputRecords.values());
+
+            writeCsvFromArrayList(outputRecordList);
+
         } catch (IOException e) {
             e.printStackTrace();
         }
