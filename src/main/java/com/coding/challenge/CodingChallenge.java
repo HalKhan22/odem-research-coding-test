b3d9d4ecb60603ae46cc9ad9dfe60faff50f1aef
Haleema Khatoon <86432187+HalKhan22@users.noreply.github.com>
2023-12-22 22:46:20 +0000

    Update CodingChallenge.java

--- src/main/java/com/coding/challenge/CodingChallenge.java
+++ src/main/java/com/coding/challenge/CodingChallenge.java
@@ -12,36 +12,47 @@ import java.util.Scanner;
 
 public class CodingChallenge {
     private static GenerateOutput generateOutputRecord = new GenerateOutput(new CalculateParameters(), new FileReadWrite());
-  public static void main(String[] args) {
+    private static FileReadWrite fileReadWrite = new FileReadWrite();
+
+    private static final boolean isStreaming = true;
+  public static void main(String[] args) throws URISyntaxException {
       // Create a Scanner to read from the command line
-      Scanner scanner = new Scanner(System.in);
 
-      System.out.println("Enter command-line arguments (separated by spaces):");
+      if(isStreaming){
+          fileReadWrite.processLineByLineFromInput(Paths.get( CodingChallenge.class.getClassLoader().getResource("input_data.csv").toURI()).toFile().getAbsolutePath());
 
-      // Read the entire line of input
-      String inputLine = scanner.nextLine();
+      }else{
+          Scanner scanner = new Scanner(System.in);
 
-      // Split the input line into an array of strings
-      String[] arguments = inputLine.split("\\s+");
+          System.out.println("Enter command-line arguments (separated by spaces):");
 
-      // Check if there are any arguments
-      if (arguments.length > 0) {
-          URL res = CodingChallenge.class.getClassLoader().getResource(arguments[0]);
-          File file = null;
-          try {
-              file = Paths.get(res.toURI()).toFile();
-          } catch (URISyntaxException e) {
-              System.out.println("No such file");
+          // Read the entire line of input
+          String inputLine = scanner.nextLine();
+
+          // Split the input line into an array of strings
+          String[] arguments = inputLine.split("\\s+");
+
+          // Check if there are any arguments
+          if (arguments.length > 0) {
+              URL res = CodingChallenge.class.getClassLoader().getResource(arguments[0]);
+              File file = null;
+              try {
+                  file = Paths.get(res.toURI()).toFile();
+              } catch (URISyntaxException e) {
+                  System.out.println("No such file");
+              }
+
+              String absolutePath = file.getAbsolutePath();
+              generateOutputRecord.generateOutput(absolutePath);
+
+          } else {
+              System.out.println("No command-line arguments provided.");
           }
 
-          String absolutePath = file.getAbsolutePath();
-          generateOutputRecord.generateOutput(absolutePath);
-
-      } else {
-          System.out.println("No command-line arguments provided.");
+          // Close the scanner
+          scanner.close();
       }
 
-      // Close the scanner
-      scanner.close();
+
   }
 }
